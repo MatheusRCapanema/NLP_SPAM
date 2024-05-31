f866b91f390572efba50278dcb4596aff8983248
MatheusRCapanema <matheus.rocap@gmail.com>
2024-05-31 11:32:25 -0300

    Projeto para identificar SPAM

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import os
+
 import streamlit as st
 import pickle
 
-model = pickle.load(open('model.pkl', 'rb'))
+
+base_path = os.path.dirname(__file__)
+file_path = os.path.join(base_path, 'model.pkl')
+if os.path.exists(file_path):
+    model = pickle.load(open(file_path, 'rb'))
+else:
+    print(f"File {file_path} does not exist.")
 
 st.title("Predição se uma mensagem é um spam")
 
